2493fa4d34418cd72b6223f074bf8ceb37a51619
Truong Duc Huy <hplands@hotmail.com>
2017-12-21 22:52:50 +0700

    commit 2

---
+++ FormAuthDemo/FormAuth.Website/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormAuth.Website.Models
+{
+    public class User
+    {
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
